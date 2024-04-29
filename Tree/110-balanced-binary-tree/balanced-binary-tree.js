ebe0845d76600313638c61cf6d6bc740cd8d7b8d
BEJOY K K <kkbejoy@ymail.com>
2024-04-29 13:26:26 +0530

    Time: 66 ms (48.03%) | Memory: 53.5 MB (95.62%) - LeetSync

---
+++ Tree/110-balanced-binary-tree/balanced-binary-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+
+var isBalanced = function (root) {
+    if (!root) return true
+    let balanced = true;
+
+    const dfs = (root) => {
+        if (!root) return 0;
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+
+        if (Math.abs(left - right) > 1) balanced = false
+
+        return Math.max(left, right) + 1
+    }
+    dfs(root);
+    return balanced
+};
+
