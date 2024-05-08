612253db3601239952f8d7a00a370c7281cc3a83
BEJOY K K <kkbejoy@ymail.com>
2024-05-08 12:14:40 +0530

    Time: 53 ms (99.46%) | Memory: 53.5 MB (32.80%) - LeetSync

--- May-24/506-relative-ranks/relative-ranks.js
+++ May-24/506-relative-ranks/relative-ranks.js
@@ -3,12 +3,15 @@
  * @return {string[]}
  */
 var findRelativeRanks = function(score) {
-    let answer= []
-    let sortted=[...score]
-    sortted.sort((a,b)=>b-a)
+    
+    let sortted=[...score].sort((a,b)=>b-a)
+    const map= new Map()
     const ranks=["Gold Medal","Silver Medal","Bronze Medal"]
+    let rank=0
     for(let i=0;i<score.length;i++){
-        answer[score.indexOf(sortted[i])]=(i<3?ranks[i]:`${i+1}`)
+        map.set(sortted[i],i<3? ranks[i] : `${rank+1}`)
+        rank++
     }
+    const answer=score.map((s)=>map.get(s))
     return answer
 };
