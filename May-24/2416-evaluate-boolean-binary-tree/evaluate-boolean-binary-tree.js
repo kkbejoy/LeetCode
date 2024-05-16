55f3126678fb361d4f1f14ee0c10b4ee316a7b2e
BEJOY K K <kkbejoy@ymail.com>
2024-05-16 13:23:14 +0530

    Time: 54 ms (90.48%) | Memory: 55.9 MB (25.93%) - LeetSync

---
+++ May-24/2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function (root) {
+    const evaluvate = (node) => {
+        if (node.val === 0 || node.val === 1) return node.val === 1
+        else if (node.val === 2) return (evaluvate(node.left) || evaluvate(node.right))
+        else if (node.val === 3) return (evaluvate(node.left) && evaluvate(node.right))
+    }
+    return evaluvate(root)
+};
