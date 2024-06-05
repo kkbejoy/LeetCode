330ea21b7916dd51dd23976e4b3c83c0c92fdb76
BEJOY K K <kkbejoy@ymail.com>
2024-06-06 00:17:14 +0530

    Time: 50 ms (93.96%) | Memory: 51.4 MB (76.37%) - LeetSync

--- Jun-24/1044-find-common-characters/find-common-characters.js
+++ Jun-24/1044-find-common-characters/find-common-characters.js
@@ -3,32 +3,16 @@
  * @return {string[]}
  */
 var commonChars = function (words) {
-    const map = new Map()
-    const base = words[0]
-    for (let i = 0; i < base.length; i++) {
-        if (map.has(base[i])) {
-            let value = map.get(base[i])
-            map.set(base[i], ++value);
-        }
-        else map.set(base[i], 1)
-    }
-    for (let i = 1; i < words.length; i++) {
-        let count = {};
-        let word = words[i]
-        for (let j = 0; j < word.length; j++) {
-            count[word[j]] = count[word[j]] + 1 || 1
-        }
-        for ([key, value] of map) {
-            if (count[key]) map.set(key, Math.min(value, count[key]) || 0)
-            else map.delete(key)
-        }
-    }
-    const result = []
-    for ([key, value] of map) {
-        for (let i = 0; i < value; i++) {
-            result.push(key)
-        }
-    }
+    let result = []
 
+    let initial = words[0];
+
+    initial.split("").forEach((char, index) => {
+        if (words.every(word => word.includes(char))) {
+            result.push(char);
+            words = words.map(word => word.replace(char, ""))
+
+        }
+    })
     return result
 };
