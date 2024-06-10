2a619b4757034ba3b9c888ee597a9fbe11f0c87c
BEJOY K K <kkbejoy@ymail.com>
2024-06-10 11:45:34 +0530

    Time: 47 ms (87.11%) | Memory: 48.8 MB (76.17%) - LeetSync

--- Jun-24/1137-height-checker/height-checker.js
+++ Jun-24/1137-height-checker/height-checker.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var heightChecker = function (heights) {
-    const expected = JSON.parse(JSON.stringify(heights))
+    const expected = heights.map((height)=>height)
     expected.sort((a, b) => a - b)
     let count = 0
     for (let i = 0; i < expected.length; i++) {
