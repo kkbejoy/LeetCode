ddb8dab2e00e033c575050551300e0ce7c45d2ba
BEJOY K K <kkbejoy@ymail.com>
2024-08-05 22:28:38 +0530

    Time: 55 ms (91.75%) | Memory: 51.8 MB (68.56%) - LeetSync

--- Jun-24/2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.js
+++ Jun-24/2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.js
@@ -4,15 +4,19 @@
  * @return {string}
  */
 var kthDistinct = function (arr, k) {
-    let distinctCount = 0;
+    const map = new Map();
     for (let i = 0; i < arr.length; i++) {
-        if (isDistinct(arr, i)) distinctCount++
-        if (distinctCount == k) return arr[i]
+        let wordCount = map.get(arr[i])
+        if (!wordCount) map.set(arr[i], 1);
+        else map.set(arr[i], ++wordCount);
+    }
+    let uniqueCount = 0
+    for (let [key, value] of map) {
+        if (value > 1) continue;
+        uniqueCount++
+        if (uniqueCount === k) return key
     }
     return ""
 };
 
 
-const isDistinct = (arr, i) => {
-    return arr.filter(word => word === arr[i]).length === 1
-}
