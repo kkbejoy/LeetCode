791338b5ec9e611bc78d6aadc939cad0dd1ce66f
BEJOY K K <kkbejoy@ymail.com>
2024-08-07 00:03:48 +0530

    Time: 66 ms (50.65%) | Memory: 52.1 MB (25.54%) - LeetSync

--- Aug-24/3221-find-the-peaks/find-the-peaks.js
+++ Aug-24/3221-find-the-peaks/find-the-peaks.js
@@ -4,6 +4,7 @@
  */
 var findPeaks = function (mountain) {
     const peaks = [];
+    if (mountain.length <= 2) return peaks
     for (let i = 0; i < mountain.length; i++) {
         if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) peaks.push(i)
     }
