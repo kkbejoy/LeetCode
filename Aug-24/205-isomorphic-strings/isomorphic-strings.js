62e17a13ecdaf7979c21bcea2d0b4001ea04528c
BEJOY K K <kkbejoy@ymail.com>
2024-08-10 23:29:06 +0530

    Time: 63 ms (52.10%) | Memory: 51.2 MB (37.44%) - LeetSync

--- Aug-24/205-isomorphic-strings/isomorphic-strings.js
+++ Aug-24/205-isomorphic-strings/isomorphic-strings.js
@@ -4,9 +4,16 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
-    if(s.length!==t.length) return false
-    for(let i=0;i<s.length;i++){
-        if(s.indexOf(s[i])!==t.indexOf(t[i])) return false
+    const sMap = {};
+    const tMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!sMap[s[i]] && !tMap[t[i]]) {
+            sMap[s[i]] = t[i]
+            tMap[t[i]] = s[i]
+        } else if (sMap[s[i]] !== t[i] || tMap[t[i]] !== s[i]) {
+            return false
+        }
     }
     return true
 };
