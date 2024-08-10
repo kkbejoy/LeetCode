bd411b8768306fbafd5f8655ed81df1f1c5e9462
BEJOY K K <kkbejoy@ymail.com>
2024-08-10 23:46:44 +0530

    Time: 65 ms (42.05%) | Memory: 50.9 MB (44.51%) - LeetSync

--- Aug-24/205-isomorphic-strings/isomorphic-strings.js
+++ Aug-24/205-isomorphic-strings/isomorphic-strings.js
@@ -4,16 +4,17 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
-    const sMap = {};
-    const tMap = {};
+    if(s.length!==t.length) return false
+    const sMap = new Map();
+    const tMap = new Map();
 
     for (let i = 0; i < s.length; i++) {
-        if (!sMap[s[i]] && !tMap[t[i]]) {
-            sMap[s[i]] = t[i]
-            tMap[t[i]] = s[i]
-        } else if (sMap[s[i]] !== t[i] || tMap[t[i]] !== s[i]) {
-            return false
-        }
+       if(sMap.has(s[i]) && sMap.get(s[i])!==t[i]) return false
+       else if(tMap.has(t[i]) && tMap.get(t[i])!==s[i]) return false
+       else if(!sMap.has(s[i])&& !tMap.has(t[i])){
+        sMap.set(s[i],t[i]);
+        tMap.set(t[i],s[i])
+       }
     }
     return true
 };
