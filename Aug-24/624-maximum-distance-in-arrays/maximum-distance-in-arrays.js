0dcc2a1fe2035285a945cb1fd85bab9fef3b3071
BEJOY K K <kkbejoy@ymail.com>
2024-08-17 00:04:50 +0530

    Time: 106 ms (93.75%) | Memory: 67.8 MB (62.50%) - LeetSync

---
+++ Aug-24/624-maximum-distance-in-arrays/maximum-distance-in-arrays.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function (arrays) {
+    let globalMin = arrays[0][0];
+    let globalMax = arrays[0][arrays[0].length - 1];
+    let maxDistance = 0;
+
+    for (let i = 1; i < arrays.length; i++) {
+        let min = arrays[i][0]
+        let max = arrays[i][arrays[i].length - 1]
+        maxDistance = Math.max(maxDistance, Math.abs(max - globalMin))
+        maxDistance = Math.max(maxDistance, Math.abs(min - globalMax))
+
+        globalMax = Math.max(globalMax, max)
+        globalMin = Math.min(globalMin, min)
+
+    }
+        console.log(globalMin, globalMax)
+
+    return maxDistance
+};
