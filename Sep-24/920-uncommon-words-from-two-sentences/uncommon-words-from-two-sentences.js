cd0ae7dca020ba56e737be62c45ddf4c4c02a328
BEJOY K K <kkbejoy@ymail.com>
2024-09-17 20:04:40 +0530

    Time: 71 ms (5.74%) | Memory: 49 MB (86.07%) - LeetSync

--- Sep-24/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.js
+++ Sep-24/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.js
@@ -4,10 +4,17 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function (s1, s2) {
-   const combined=s1.split(" ").concat(s2.split(" "))
-   const unCommon=[]
-   for(let i=0;i<combined.length;i++){
-    if(combined.lastIndexOf(combined[i])===combined.indexOf(combined[i]))unCommon.push(combined[i])
-   }
-    return unCommon
+  const combined=s1.split(" ").concat(s2.split(" "))
+  const map=new Map();
+
+  for(let i=0;i<combined.length;i++){
+    const word=combined[i]
+    if(map.has(word)) map.set(word,map.get(word)+1)
+    else map.set(word,1)
+  }
+  const unCommon=[]
+  map.forEach((value,key)=>{
+    if(value===1) unCommon.push(key)
+  })
+  return unCommon
 };
