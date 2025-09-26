ba869f45d681cb4ad0968dbafd8e71d6f20b20b9
BEJOY K K <kkbejoy@ymail.com>
2025-09-26 17:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (93.62%) - LeetSync

--- July-24/66-plus-one/plus-one.js
+++ July-24/66-plus-one/plus-one.js
@@ -3,37 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-
-    let allDigits = true
-    for (let i = 0; i < digits.length; i++) {
-        if (digits[i] !== 9) {
-            allDigits = false
-            break;
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits
         }
+        digits[i]=0;
     }
-    if (allDigits) {
-        for (let i =1; i < digits.length; i++) {
-            digits[i] = 0    
-        }
-        digits[0] = 1
-        digits[digits.length] = 0
-        return digits
-    }
-    return recursiveCorrection(digits, digits.length - 1)
+    digits.unshift(1)
+    return digits
 };
 
-const recursiveCorrection = (digits, position) => {
-    if(position<0)return digits
-    if (digits[position] !== 9) {
-        digits[position] = digits[position] + 1
-        return digits
-    }
-   else  if(position===0){
-        digits[0]= digits[0]+1
-        return digits
-    }
-    else {
-        digits[position]= 0
-        recursiveCorrection(digits, position - 1)}
-    return digits
-}
