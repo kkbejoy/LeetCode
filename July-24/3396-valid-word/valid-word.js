1db247e63ecfdc50b2bc12cb9ba204386b1e8598
BEJOY K K <kkbejoy@ymail.com>
2025-10-06 22:52:31 +0530

    Time: 4 ms (5.33%) | Memory: 55.6 MB (48.00%) - LeetSync

---
+++ July-24/3396-valid-word/valid-word.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function (word) {
+    const vowels = ["a", "e", "i", "o", "u"]
+    const symbols = ["@", "#", "$"]
+    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const minLength = 3;
+    let isVowel = false;
+    let isConsonant = false;
+    if (word.length < minLength) return false;
+    for (let i = 0; i < word.length; i++) {
+        if (symbols.includes(word[i])) return false
+        else if (digits.includes(Number(word[i]))) continue
+            // console.log(word[i],digits.includes(word[i].toString()))
+
+         if (vowels.includes(word[i].toLowerCase())) isVowel = true
+        else isConsonant = true
+        // else if()
+    }
+    console.log(isVowel, isConsonant)
+    return isVowel && isConsonant
+
+};
