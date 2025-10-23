b5b012971ae44cd53c031f19cf568379b595b2f1
BEJOY K K <kkbejoy@ymail.com>
2025-10-23 22:29:35 +0530

    Time: 9 ms (45.09%) | Memory: 64.1 MB (6.65%) - LeetSync

--- July-24/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ July-24/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,9 +3,18 @@
  * @return {number[]}
  */
 var sortedSquares = function (nums) {
+    const result = []
+    let right = nums.length - 1;
+    let left = 0
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (Math.pow(nums[left], 2) > Math.pow(nums[right], 2)) {
+            result[i] = (Math.pow(nums[left], 2))
+            left++
+        } else {
+            result[i] = (Math.pow(nums[right], 2))
+            right--
+        }
 
-    const squares = nums.map(num => Math.pow(num, 2)).sort((a, b) => a - b)
-    console.log(squares)
-    return squares
-    // return sorttedSquares
+    }
+    return result
 };
