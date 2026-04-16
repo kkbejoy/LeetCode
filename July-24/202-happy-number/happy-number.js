ce99fed8c143b0b2d3afebb93bd3470844ec16de
BEJOY K K <kkbejoy@ymail.com>
2026-04-16 23:19:46 +0530

    Time: 1 ms (75.50%) | Memory: 54 MB (91.74%) - LeetSync

---
+++ July-24/202-happy-number/happy-number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+const isHappy  = (n) => {
+    let slow = n;
+    fast = getSquare(n)
+
+    while (slow !== 1 && fast !== slow) {
+        slow = getSquare(slow)
+        fast = getSquare(getSquare(fast))
+    }
+    return fast === 1
+}
+
+const getSquare = (num) => {
+    let sum = 0
+    while (num > 0) {
+        const digit = num % 10;
+        sum += Math.pow(digit, 2)
+        num = Math.floor(num / 10)
+    }
+    return sum
+}
